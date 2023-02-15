8d3bb6eb76b77dcf73b384d3ee6884c1a8cd40a9
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-02-15 16:12:17 +0900

    JpaMain class 수정

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,47 +1,39 @@
 package hellojpa;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
 public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("A");
+            // 저장
+            Team team =  new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-            Member member2 = new Member();
-            member1.setUsername("B");
+            Member member = new Member();
+            member.setUsername("member1");
+            // 이렇게 해주면 JPA가 알아서 PK값을 team에서 꺼내서 FK값에 insert해준다.
+            member.setTeam(team);
+            em.persist(member);
 
-            Member member3 = new Member();
-            member1.setUsername("C");
+            em.flush();
+            em.clear();
 
-            System.out.println("=================");
+            Member findMember = em.find(Member.class, member.getId());
+            List<hellojpa.Member> members = findMember.getTeam().getMembers();
 
-            em.persist(member1); // 1, 51
-            em.persist(member2); // MEMORY
-            em.persist(member3); // MEMORY
-
-            System.out.println("member1 = " + member1.getId());
-            System.out.println("member2 = " + member2.getId());
-            System.out.println("member3 = " + member3.getId());
-            System.out.println("=================");
 
             tx.commit();
         } catch (Exception e) {
@@ -49,6 +41,7 @@ public class JpaMain {
         } finally {
             em.close();
         }
+
         emf.close();
     }
 }
